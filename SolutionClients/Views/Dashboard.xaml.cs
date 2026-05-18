734e1f0eaef974602db37b974b0d16783a5a2a28
Housni0 <housniissaassoumani@gmail.com>
2026-05-18 09:00:13 +0200

    first commit

---
+++ SolutionClients/Views/Dashboard.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SolutionClients.Views
+{
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SolutionClients/Views/LoginWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace SolutionClients.Views
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+
+            this.Close();
+        }
+    }
+}
