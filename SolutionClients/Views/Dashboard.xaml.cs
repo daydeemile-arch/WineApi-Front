31ba2bc9b7fcde72cdce2e846ffbd4ea8dc32240
daydeemile-arch <daydeemile@gmail.com>
2026-05-19 11:20:01 +0200

    Login API (#3)
    Co-authored-by: Yassin Riahi <yassin.riahi@viacesi.fr>

--- SolutionClients/Views/Dashboard.xaml.cs
+++ SolutionClients/Views/Dashboard.xaml.cs
@@ -1,44 +1,57 @@
-﻿using System.Windows;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Windows;
 
 namespace SolutionClients.Views
 {
     public partial class Dashboard : Window
     {
-        public Dashboard()
+        private readonly HttpClient _httpClient;
+        private readonly string _token;
+
+        public Dashboard(HttpClient httpClient, string token)
         {
             InitializeComponent();
+            _httpClient = httpClient;
+            _token = token;
+
+            // S'assure que le token est bien dans les headers
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", _token);
+
+            // Ouvre Wines par défaut au démarrage
+            MainFrame.Content = new WinesView(_httpClient);
         }
 
         private void BtnWines_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new WinesView();
+            MainFrame.Content = new WinesView(_httpClient);
         }
 
         private void BtnCustomers_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new CustomersView();
+            MainFrame.Content = new CustomersView(_httpClient);
         }
 
         private void BtnSuppliers_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new SuppliersView();
+            MainFrame.Content = new SuppliersView(_httpClient);
         }
 
         private void BtnSales_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new SalesView();
+            MainFrame.Content = new SalesView(_httpClient);
         }
 
         private void BtnStock_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new StockView();
+            MainFrame.Content = new StockView(_httpClient);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            LoginWindow login = new LoginWindow();
+            var login = new LoginWindow();
             login.Show();
-
             this.Close();
         }
     }
